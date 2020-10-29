f71f2f009cf93272df73cd69b5e7eac7d435490f
mala23 <mail@mala23.me>
2020-10-29 07:41:47 +0100

    create nav

--- frontend/components/nav/index.js
+++ frontend/components/nav/index.js
@@ -1,22 +1,74 @@
+import { Component } from 'react'
 import Link from 'next/link'
-import styled from 'styled-components'
-import { StyledNav, StyledNavList, StyledNavToggle } from './styles'
+import { slide as Menu  } from 'react-burger-menu'
 
-const Nav = () => (
-  <StyledNav>
-    <StyledNavToggle>...</StyledNavToggle>
-    <StyledNavList>
-      <Link href="/create">
-        <a>Create your poem</a>
-      </Link>
-      <Link href="/see">
-        <a>See poems</a>
-      </Link>
-      <Link href="#">
-        <a>Follow</a>
-      </Link>
-    </StyledNavList>
-  </StyledNav>
-)
+var styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    right: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%',
+    width: '100%'
+  },
+  bmMenu: {
+    background: 'white',
+    fontSize: '1.15em',
+    margin: '0',
+    width: '100vw'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmItem: {
+    display: 'inline-block'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+
+class Nav extends Component {
+    showSettings (event) {
+      event.preventDefault();
+    }
+
+    render() {
+      return(
+        <Menu styles={ styles }>
+          <Link href="/create">
+            <a>Create your poem</a>
+          </Link>
+          <Link href="/see">
+            <a>See poems</a>
+          </Link>
+          <Link href="#">
+            <a>Follow</a>
+          </Link>
+          <a onClick={ this.showSettings  } className="menu-item--small" href="">Settings</a>
+        </Menu>
+  )}
+}
 
 export default Nav
