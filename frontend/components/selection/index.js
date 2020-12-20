03aa72fcad4eadacec3c709d6d201b9dbef43d8b
Luca Nils Schmid <me@lucaschmid.net>
2020-12-20 17:54:41 +0100

    implement "reset all" button

--- frontend/components/selection/index.js
+++ frontend/components/selection/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import SelectionPlate from '../selection-plate'
 import { StyledSelection, StyledSelectionPlates, StyledSelectionImage, StyledDropZone, StyledSelectionNote, StyledPlaceholderDropZone } from './styles'
+import SelectionControls from '../selection-controls'
 import { useDrop } from 'react-dnd'
 import { v4 as uuid } from 'uuid'
 
@@ -24,6 +25,9 @@ const Selection  = () => {
       canDrop: monitor.canDrop(),
     })
   })
+  const handleReset = () => {
+    setPlates([])
+  }
 
   const handleMove = (item, oldIndex, newIndex) => {
     const newPlates = [...plates]
@@ -35,22 +39,25 @@ const Selection  = () => {
   }
 
   return (
-    <StyledSelection>
-      <StyledSelectionPlates>
-        {plates.length ? (
-          <React.Fragment>
-            {plates.map((plate, i) => (
-              <SelectionPlate key={plate.id} index={i} onMove={handleMove} dropable plate={plate}/>
-            ))}
-            <StyledDropZone innerRef={drop} active={canDrop}/>
-          </React.Fragment>
-        ) : (
-          <StyledPlaceholderDropZone innerRef={drop} active={canDrop}>
-            <StyledSelectionNote>{'Drag and drop your selected plates here.'}</StyledSelectionNote>
-          </StyledPlaceholderDropZone>
-        )}
-      </StyledSelectionPlates>
-    </StyledSelection>
+    <React.Fragment>
+      <StyledSelection>
+        <StyledSelectionPlates>
+          {plates.length ? (
+            <React.Fragment>
+              {plates.map((plate, i) => (
+                <SelectionPlate key={plate.id} index={i} onMove={handleMove} dropable plate={plate}/>
+              ))}
+              <StyledDropZone innerRef={drop} active={canDrop}/>
+            </React.Fragment>
+          ) : (
+            <StyledPlaceholderDropZone innerRef={drop} active={canDrop}>
+              <StyledSelectionNote>{'Drag and drop your selected plates here.'}</StyledSelectionNote>
+            </StyledPlaceholderDropZone>
+          )}
+        </StyledSelectionPlates>
+      </StyledSelection>
+      <SelectionControls onReset={handleReset} />
+    </React.Fragment>
   )
 }
 
--- frontend/pages/create.js
+++ frontend/pages/create.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Link from 'next/link'
 
 import Selection from '../components/selection'
-import SelectionControls from '../components/selection-controls'
 import Plates from '../components/plates'
 import DragPreview from '../components/dragpreview'
 
@@ -10,7 +9,6 @@ const Create = props => (
   <div>
     <DragPreview/>
     <Selection />
-    <SelectionControls />
     <Plates />
   </div>
 )
